2b9a6e23075d320e9d036c8530df351c3cffbc5b
YehYu-Rabbit <71476355+YehYu-Rabbit@users.noreply.github.com>
2021-02-25 15:31:32 +0800

    Add files via upload

---
+++ HelloWorld2.java
@@ -0,0 +1,8 @@
+public class HelloWorld2{
+
+     public static void main(String []args){
+        System.out.print("Hello ");
+	System.out.print("Mydeargreatteacher");
+	System.out.println("Hello World");
+     }
+}
---
+++ HelloWorld3.java
@@ -0,0 +1,8 @@
+public class HelloWorld3{
+
+     public static void main(String []args){
+        System.out.print("Hello ");
+	System.out.println("Mydeargreatteacher");
+	System.out.println("Hello World");
+     }
+}
